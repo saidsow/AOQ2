e4b1a3da35beb64183e7403f812b6e41aa7c74ca
Said Sowma <saiddsowma@gmail.com>
2024-11-13 16:47:28 -0500

    css update

--- scripts.js
+++ scripts.js
@@ -2,4 +2,9 @@ function toggleAccordion(header) {
     const content = header.nextElementSibling;
     header.classList.toggle("active");
     content.classList.toggle("active");
+    
+    // Reset opacity on close in mobile view
+    if (window.innerWidth <= 768) {
+        content.style.opacity = content.classList.contains("active") ? "1" : "0.8";
+    }
 }
