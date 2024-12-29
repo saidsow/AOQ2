2311348df60e4c50c7de7744d95388a2582c9ea1
Said Sowma <saiddsowma@gmail.com>
2024-12-28 20:42:48 -0500

    update

--- test/test.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", function() {
-    setTimeout(() => {
-        document.body.classList.remove("preload");
-    }, 100000); // Adjust delay as desired (1000ms = 1s)
-});
